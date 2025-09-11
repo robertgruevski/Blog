436f027e279c6333a622e880dc4e6119d6bcfa4e
robertgruevski <robertgruevski@outlook.com>
2025-09-11 16:10:07 -0400

    Edit or Update Tag Functionality

--- Blog.Web/Controllers/TagsController.cs
+++ Blog.Web/Controllers/TagsController.cs
@@ -5,34 +5,70 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers
 {
-	public class TagsController : Controller
-	{
-		private readonly BlogDbContext context;
-		public TagsController(BlogDbContext context)
-		{
-			this.context = context;
-		}
+    public class TagsController : Controller
+    {
+        private readonly BlogDbContext context;
+        public TagsController(BlogDbContext context)
+        {
+            this.context = context;
+        }
 
-		[HttpGet]
-		public IActionResult Add()
-		{
-			return View();
-		}
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
 
         [HttpPost]
         public IActionResult Add(AddTagRequest addTagRequest)
         {
-			context.Tags.Add(new Tag
-			{
+            context.Tags.Add(new Tag
+            {
                 Name = addTagRequest.Name,
                 DisplayName = addTagRequest.DisplayName
             });
-			context.SaveChanges();
+            context.SaveChanges();
 
             return RedirectToAction(nameof(List));
         }
 
-		[HttpGet]
-		public IActionResult List() => View(context.Tags.ToList());
+        [HttpGet]
+        public IActionResult List() => View(context.Tags.ToList());
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var tag = context.Tags.FirstOrDefault(x => x.Id == id);
+            if (tag is not null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+
+                return View(editTagRequest);
+            }
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest)
+        {
+            var tag = context.Tags.FirstOrDefault(x => x.Id == editTagRequest.Id);
+
+            if (tag is not null)
+            {
+                tag.Name = editTagRequest.Name;
+                tag.DisplayName = editTagRequest.DisplayName;
+
+                context.SaveChanges();
+
+                return RedirectToAction(nameof(List));
+            }
+
+            return View(editTagRequest);
+        }
     }
 }
