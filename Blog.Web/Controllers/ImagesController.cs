bc329bc2411951753418561550ffa299f8a7dfad
robertgruevski <robertgruevski@outlook.com>
2025-09-11 21:14:31 -0400

    Create Image API

---
+++ Blog.Web/Controllers/ImagesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ImagesController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return Ok("This is the GET Images endpoint.");
+		}
+	}
+}
