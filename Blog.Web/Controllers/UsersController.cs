d24c0b0594b37e4a2d307703a306591095f2b36e
robertgruevski <robertgruevski@outlook.com>
2025-09-13 15:49:01 -0400

    Create New User

--- Blog.Web/Controllers/UsersController.cs
+++ Blog.Web/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 ﻿using Blog.Web.Models.ViewModels;
 using Blog.Web.Repositories.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
 
 namespace Blog.Web.Controllers
 {
@@ -9,11 +11,15 @@ namespace Blog.Web.Controllers
 	public class UsersController : Controller
 	{
 		private readonly IUserRepository userRepository;
+		private readonly UserManager<IdentityUser> userManager;
 
-		public UsersController(IUserRepository userRepository)
+		public UsersController(IUserRepository userRepository, UserManager<IdentityUser> userManager)
 		{
 			this.userRepository = userRepository;
+			this.userManager = userManager;
 		}
+
+		[HttpGet]
 		public async Task<IActionResult> List()
 		{
 			var users = await userRepository.GetAll();
@@ -31,6 +37,40 @@ namespace Blog.Web.Controllers
 			}
 
 			return View(usersViewModel);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> List(UserViewModel request)
+		{
+			var identityUser = new IdentityUser
+			{
+				UserName = request.UserName,
+				Email = request.Email
+			};
+
+			var identityResult = await userManager.CreateAsync(identityUser, request.Password);
+
+			if(identityResult is not null)
+			{
+				if(identityResult.Succeeded)
+				{
+					var roles = new List<string> { "User" };
+
+					if(request.IsAdmin)
+					{
+						roles.Add("Admin");
+					}
+
+					identityResult = await userManager.AddToRolesAsync(identityUser, roles);
+
+					if(identityResult is not null && identityResult.Succeeded)
+					{
+						return RedirectToAction("List", "Users");
+					}
+				}
+			}
+
+			return null;
 		}
 	}
 }
--- Blog.Web/Models/ViewModels/UserViewModel.cs
+++ Blog.Web/Models/ViewModels/UserViewModel.cs
@@ -3,5 +3,9 @@
 	public class UserViewModel
 	{
 		public List<User> Users { get; set; }
+		public string UserName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public bool IsAdmin { get; set; }
 	}
 }
